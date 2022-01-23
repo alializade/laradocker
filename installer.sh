182894a1494fd6704f24297e4ac4453a4920578d
Ali Alizade <ali.alizade@outlook.com>
2022-01-23 20:38:24 +0330

    Fetch installer using git

--- installer.sh
+++ installer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-docker info >/dev/null 2>&1
-
 # Ensure that Docker is running...
-if [ $? -ne 0 ]; then
+if docker info >/dev/null 2>&1 -ne 0; then
   echo "Docker is not running."
 
   exit 1
 fi
 
 echo "Enter your project name:"
-reas NAME
+read -r NAME
+mkdir NAME && cs NAME
+
+docker run -it --rm -v ${pwd}:/www/html/${NAME}.com/ alpine/git clone git@github.com:alializade/laradocker.git
 
 #docker run --rm \
 #    -v "$(pwd)":/opt \
@@ -18,17 +19,20 @@ reas NAME
 #    laravelsail/php81-composer:latest \
 #    bash -c "laravel new exam && cd exam && php ./artisan sail:install --with=mysql,redis,meilisearch,mailhog,selenium "
 
+# sed for composer
+# sed for .env file
+
 cd "$NAME" || exit
 
 echo ""
 
-CYAN='\033[0;36m'
-LIGHT_CYAN='\033[1;36m'
+#CYAN='\033[0;36m'
+#LIGHT_CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-sudo -n true 2>/dev/null
-if $? eq 0; then
+
+if sudo -n true 2>/dev/null eq 0; then
   sudo chown -R "$USER": .
 else
   echo -e "${WHITE}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
